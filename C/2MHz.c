265224ca7d67f5d46b1ea6a84098ea117567a5fc
Takayama-Lissajous <fpga114@gmail.com>
2018-01-03 16:01:33 +0900

    2018-01-03

---
+++ C/2MHz.c
@@ -0,0 +1,25 @@
+/*
+2.
+Explain the program operation principle and speed for the following two programs.
+Make C program example and verify it.
+
+    [function declaration)]	int square (int A);
+    [function Call)]	int A;
+    		A = square (A);
+
+    [function declaration)]	void sqaure (int *A);
+    [function Call)]	int A;
+    		square (&A);
+*/
+//what second it is when we convert from 2Mhz.
+// Important : Should not use Calculator!
+// Answer :
+
+//Convert 1Mhz to Khz and Hz.
+//Important : Do not write Arabic number. Write in English.
+// Answer :
+int main()
+{
+
+  return 0;
+}
